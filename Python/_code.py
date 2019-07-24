c17284bbd4e77ec5a07b4a00eb85ef54cf3a5b1b
MrAsieru <asiescp@gmail.com>
2019-07-24 13:31:23 +0200

    Commit

--- Python/_code.py
+++ Python/_code.py
@@ -1,2 +1,21 @@
-txt = "Invertir esta frase"
-print(txt)
+txt = "Un texto para probar a este algoritmo de aqui"
+
+def transform(str):
+    sol = ""
+    str = str.lower()
+    for i in range(0, len(str)):
+        if str[i] != " ": sol = sol + str[i]
+    print("Sol: %s" % sol)
+    return sol
+
+def code(str):
+    leng = int(len(str) / 2)
+    parejas = {}
+    for i in range(0, leng):
+        nStr = str[i*2] + str[i*2+1]
+        if nStr in parejas:
+            parejas[nStr] += 1
+        else:
+            parejas[nStr] = 1
+    return parejas
+print (code(transform(txt)))
